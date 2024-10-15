5c362efa8eda1279c383bca1533254a1df89c2ac
Vaishnavi-Thorve <67958255+Vaishnavi-Thorve@users.noreply.github.com>
2024-10-15 17:48:21 +0530

    Fixed works for all dx values

--- plot_with_bcs.py
+++ plot_with_bcs.py
@@ -77,7 +77,7 @@ class Dirichlet_Neumann:
                     self.send_data(room, temperature_grid)
                     print(f'The u received from iteration {i} is {u}')
                 else: pass
-        return locals().get('sol_relax', None)
+        return sol_relax
             
     def send_data(self, room, temperature_grid):
         for direction, info in room.adjacent_rooms.items():
@@ -90,11 +90,11 @@ class Dirichlet_Neumann:
 
             if direction == 'right':
                 print(f'Rank {self.rank} sending right to rank {adjacent_rank}')
-                print(f'The elements sent to right with temperature_grid is: {temperature_grid[:, -1]}')
+                print(f'The elements sent to right with temperature_grid is: {temperature_grid[start:end, -2:]}')
                 self.comm.send(temperature_grid[start:end, -2:], dest = adjacent_rank,  tag = 100 + self.rank)
             if direction == 'left':
                 print(f'Rank {self.rank} sending left to rank {adjacent_rank}')
-                print(f'The elements sent to left with temperature_grid is: {temperature_grid[:, 0]}')
+                print(f'The elements sent to left with temperature_grid is: {temperature_grid[start:end, :1]}')
                 self.comm.send(temperature_grid[start:end, :1], dest = adjacent_rank, tag = 100 + self.rank)
                 
     def receive_data(self, room, temperature_grid, dx):
@@ -127,14 +127,14 @@ class Dirichlet_Neumann:
                     print(f'flux:{flux}')
                     print(f'boundary:{room.boundary_mask[start+1:end+1, -1]}')
                     print(f'temp:{temperature_grid[start:end,-1]}')
-                    room.boundary_mask[start+1:end+1, -1] = temperature_grid[start:end,-1] + flux[1:-1] * dx
+                    room.boundary_mask[start+1:end+1, -1] = temperature_grid[start:end,-1] + flux * dx
 
                 elif direction == 'left':
                     flux = (sol_new[:, 0] - sol_new[:, -1])/ dx
                     print(f'flux:{flux}')
                     print(f'boundary:{room.boundary_mask[start+1:end+1, -1]}')
                     print(f'temp:{temperature_grid[start:end,-1]}')
-                    room.boundary_mask[start+1:end+1, 0] = temperature_grid[start:end, 0] + flux[1:-1] * dx
+                    room.boundary_mask[start+1:end+1, 0] = temperature_grid[start:end, 0] + flux * dx
                                                             
         return temperature_grid # Should we be returning this
 
@@ -170,8 +170,7 @@ def build_linalg(boundary_mask, Nx, Ny, dx):
             
             A.append(row)
             b.append(rhs)
-    print(A)
-    print(b)
+   
     return csr_matrix(np.array(A)), np.array(b) # Maybe we need to add the coefficient stuff from the previous implementation
 
 def get_index(j, k, Ny):
@@ -263,7 +262,7 @@ def plot_rooms(apartment):
     plt.savefig('Full_apartment.png')
 
 if __name__ == '__main__':
-    dx = 1 / 3
+    dx = 1 / 40
     # Solve the system of equations and display results
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -311,27 +310,28 @@ if __name__ == '__main__':
         matrix3 = temperature_values[2]  # Temperature for Room 3
 
         # Create the figure for plotting
-        fig = plt.figure(figsize=(30, 20))
+        fig = plt.figure(constrained_layout=True, figsize=(30, 20))
 
-        # Create a GridSpec with 2 rows and 3 columns, adjusting spacing to make rooms touch
-        gs = fig.add_gridspec(2, 3, width_ratios=[1, 2, 1], height_ratios=[1, 1], wspace=0, hspace=0)
+        # Create a GridSpec with 2 rows and 3 columns
+        # Room 1 (bottom-left), Room 3 (top-right), Room 2 (middle connecting both)
+        gs = fig.add_gridspec(2, 3, width_ratios=[1, 2, 1], height_ratios=[1, 1])
         print('We are plotting')
 
         # Plot Room 1 (bottom-left)
         ax1 = fig.add_subplot(gs[1, 0])
-        ax1.imshow(matrix1, cmap='plasma', aspect='auto')
+        ax1.imshow(matrix1, cmap='plasma')
         ax1.set_title("Room 1 (Bottom-left, 10x10)")
         ax1.axis('off')  # Turn off axis for better visualization
 
         # Plot Room 2 (middle-right), spanning the second column vertically
         ax2 = fig.add_subplot(gs[:, 1])  # Spanning both rows in the middle column
-        ax2.imshow(matrix2, cmap='plasma', aspect='auto')
+        ax2.imshow(matrix2, cmap='plasma')
         ax2.set_title("Room 2 (Middle, 10x20)")
         ax2.axis('off')
 
         # Plot Room 3 (top-right)
         ax3 = fig.add_subplot(gs[0, 2])
-        ax3.imshow(matrix3, cmap='plasma', aspect='auto')
+        ax3.imshow(matrix3, cmap='plasma')
         ax3.set_title("Room 3 (Top-right, 10x10)")
         ax3.axis('off')
 
